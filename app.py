a5ed0ef8a9a62555605c2444dbf2ca5f7fe5ff38
Ricardo <lucasricardo28@gmail.com>
2025-05-24 19:26:14 -0300

    Adicionando novos restaurantes

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 
+restauranteList = []
+
 def show_program_name():
     print("""
     ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
@@ -12,9 +14,9 @@ def show_program_name():
 
 
 def show_options():
-    print('1 - Cadastrar restaurante')
-    print('2 - Listar restaurante')
-    print('3 - Ativar restaurante')
+    print('1 - Cadastrar restaurante!')
+    print('2 - Listar restaurante!')
+    print('3 - Ativar restaurante!')
     print('4 - Sair \n')
 
 def exit_program():
@@ -27,12 +29,21 @@ def invalid_option():
     os.system('clear')
     main()
 
+def create_new_restaurant():
+    os.system("clear")
+    new_restaurant = input("Digite o nome do novo restaurante: ")
+    restauranteList.append(new_restaurant)
+    print(f"Novo restaurante {new_restaurant} cadastrado com sucesso! \n")
+    input("Digite uma nova tecla para iniciar novamente")
+    os.system("clear")
+    main()
+
 def choice_option():
     try:
         choiced_option = int(input('Escolha uma opção: '))
 
         if choiced_option == 1:
-            print('Cadastrar restaurante')
+            create_new_restaurant()
         elif choiced_option == 2:
             print('Listar restaurantes')
         elif choiced_option == 3:
